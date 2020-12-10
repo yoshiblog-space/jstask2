b0ef0d5f13fa00f0b0022832775883f9d08d81bd
Optimistic <optimistic@OptimisticnoMacBook-Pro.local>
2020-12-10 22:19:30 +0900

    記法、if分、スペース修正

--- js/main.js
+++ js/main.js
@@ -2,44 +2,39 @@
 
 {
   document.querySelector('button').addEventListener('click',() => {
-    const FN = document.getElementById('FizzNum');
-    const BN = document.getElementById('BuzzNum');
+    const fizzNum = document.getElementById('FizzNum');
+    const buzzNum = document.getElementById('BuzzNum');
     const ul = document.querySelector('ul');
-    const Fnum= Number(FN.value);
-    const Bnum= Number(BN.value);
+    const fizzValue= Number(fizzNum.value);
+    const buzzValue= Number(buzzNum.value);
+    const isFizzbuzzInteger = Number.isInteger(fizzValue) !== true || Number.isInteger(buzzValue) !== true;
+    const fizzbuzzNull = !fizzValue || !buzzValue ;
 
     while(ul.lastChild !== null){
       ul.removeChild(ul.lastChild);
-
     }
-
     
-    if(Number.isInteger(Fnum) !== true || Number.isInteger(Bnum) !== true){
+   if(isFizzbuzzInteger && fizzbuzzNull){
       const li = document.createElement('li');
       li.textContent = `整数値を入力してください` ;
       ul.appendChild(li);
-
-    }else if(!Fnum || !Bnum){
-      const li = document.createElement('li');
-      li.textContent = `整数値を入力してください` ;
-      ul.appendChild(li);
-
     }else{
     
       for(let i=1; i < 100 ;i++){
-        const li = document.createElement('li');
-        if( i % FN.value === 0 && i % BN.value === 0 ){
-          li.textContent = `FizzBuzz  ${i}` ;
-          ul.appendChild(li);
         
-        }else if( i % FN.value === 0){
-          li.textContent = `Fizz  ${i}` ;
-          ul.appendChild(li);
+          const li = document.createElement('li');
+          if( i % fizzValue === 0 && i % buzzValue === 0 ){
+            li.textContent = `FizzBuzz  ${i}` ;
+            ul.appendChild(li);
+        
+          }else if( i % fizzValue === 0){
+            li.textContent = `Fizz  ${i}` ;
+            ul.appendChild(li);
   
-        }else if( i % BN.value === 0){
-          li.textContent = `Buzz  ${i}` ;
-          ul.appendChild(li);
-        }
+          }else if( i % buzzValue === 0){
+            li.textContent = `Buzz  ${i}` ;
+            ul.appendChild(li);
+          }
         
       }
     }
